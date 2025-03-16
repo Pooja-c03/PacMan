b8e0ecb84a4e8a8c914ae5db38c212b94c7129e7
pooja-c03 <pooja0909.c@gmail.com>
2025-03-16 11:36:45 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+import random
+import math
+
+pygame.init()
+
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0 ,0)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 165, 0)
+CYAN = (0, 255, 255)
+WHITE = (255, 255, 255)
+PINK = (255, 192, 203)
